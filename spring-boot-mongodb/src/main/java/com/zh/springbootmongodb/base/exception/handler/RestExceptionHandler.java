8a010a341369b6502a55cdc3f25ed8421918339e
zhanghang <zhanghang9202@163.com>
2019-06-14 18:13:56 +0800

    添加mongodb子模块

--- spring-boot-mongodb/src/main/java/com/zh/springbootmongodb/base/exception/handler/RestExceptionHandler.java
+++ spring-boot-mongodb/src/main/java/com/zh/springbootmongodb/base/exception/handler/RestExceptionHandler.java
@@ -8,7 +8,6 @@ import com.zh.springbootmongodb.service.AppVisitLogService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
