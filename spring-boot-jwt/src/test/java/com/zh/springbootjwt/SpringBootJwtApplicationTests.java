a82ac60526186c368873e62b724d579d7b2314c4
zhanghang <zhanghang9202@163.com>
2019-06-25 15:47:57 +0800

    添加jwt子模块

--- spring-boot-jwt/src/test/java/com/zh/springbootjwt/SpringBootJwtApplicationTests.java
+++ spring-boot-jwt/src/test/java/com/zh/springbootjwt/SpringBootJwtApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zh.springbootwebsocket;
+package com.zh.springbootjwt;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootWebsocketApplicationTests {
+public class SpringBootJwtApplicationTests {
 
     @Test
     public void contextLoads() {
