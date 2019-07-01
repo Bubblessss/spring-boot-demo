68882ba2b2707a2fa04a3df060bdb151920dca39
zhanghang <zhanghang9202@163.com>
2019-07-01 11:37:50 +0800

    添加bloomfilter子模块

---
+++ spring-boot-bloomfilter/src/main/java/com/zh/springbootbloomfilter/service/TestService.java
@@ -0,0 +1,12 @@
+package com.zh.springbootbloomfilter.service;
+
+/**
+ * @author zhanghang
+ * @date 2019/7/1
+ */
+public interface TestService {
+
+    void printAccidentHit();
+
+    boolean checkExist(Integer num);
+}
