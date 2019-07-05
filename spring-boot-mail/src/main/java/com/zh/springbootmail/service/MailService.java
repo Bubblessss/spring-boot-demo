035c13c1e28c83ae320f522e5db34d9dea4a99d0
zhanghang <zhanghang9202@163.com>
2019-07-05 14:06:41 +0800

    添加邮件发送html和html内联图片的方法

--- spring-boot-mail/src/main/java/com/zh/springbootmail/service/MailService.java
+++ spring-boot-mail/src/main/java/com/zh/springbootmail/service/MailService.java
@@ -3,6 +3,7 @@ package com.zh.springbootmail.service;
 import org.springframework.core.io.InputStreamSource;
 
 import javax.mail.MessagingException;
+import java.util.Map;
 
 /**
  * @author zhanghang
@@ -13,5 +14,10 @@ public interface MailService {
     void sendEmail(String to, String subject, String text);
 
     void sendEmailWithFile(String to, String subject, String text, String path);
+
+    void sendWithHtml(String to, String subject, String html);
+
+    void sendWithImageHtml(String to, String subject, String html, Map<String,String> map);
+
 
 }
--- spring-boot-mail/src/main/java/com/zh/springbootmail/service/impl/MailServiceImpl.java
+++ spring-boot-mail/src/main/java/com/zh/springbootmail/service/impl/MailServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
+import java.util.Map;
 
 /**
  * @author zhanghang
@@ -65,5 +66,49 @@ public class MailServiceImpl implements MailService {
             log.error(e.getMessage(),e);
         }
 
+    }
+
+    @Override
+    public void sendWithHtml(String to, String subject, String html) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper;
+        try {
+            helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            // 设置内容，并设置内容 html 格式为 true
+            helper.setText(html, true);
+            javaMailSender.send(mimeMessage);
+            log.info("=============邮件发送成功===============");
+        } catch (MessagingException e) {
+            log.error("=============邮件发送失败===============");
+            log.error(e.getMessage(),e);
+        }
+
+    }
+
+    @Override
+    public void sendWithImageHtml(String to, String subject, String html, Map<String,String> map) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper;
+        FileSystemResource file;
+        try {
+            helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(html, true);
+            // 设置 html 中内联的图片
+            for (Map.Entry<String,String> e : map.entrySet()){
+                file = new FileSystemResource(e.getValue());
+                helper.addInline(e.getKey(), file);
+            }
+            javaMailSender.send(mimeMessage);
+            log.info("=============邮件发送成功===============");
+        } catch (MessagingException e) {
+            log.error("=============邮件发送失败===============");
+            log.error(e.getMessage(),e);
+        }
     }
 }
